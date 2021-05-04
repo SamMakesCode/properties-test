342264f7de917edd1432938195390c4162e53c34
Sam Littler <samlittler57@gmail.com>
2021-05-04 13:15:20 +0100

    Create MTC property client and relevant service provider

--- app/MTCPropertyClient.php
+++ app/MTCPropertyClient.php
@@ -6,5 +6,19 @@ use GuzzleHttp\Client;
 
 class MTCPropertyClient extends Client
 {
+    public function allProperties($callback)
+    {
+        $json = $this->getPage(1);
+        $lastPage = $json['last_page'];
+        for ($i = 1; $i < $lastPage; $i++) {
+            $callback($this->getPage($i));
+        }
+    }
 
+    public function getPage(int $page = 1): array
+    {
+        $response = $this->get('properties?page[number]=' . $page);
+        $data = $response->getBody()->getContents();
+        return json_decode($data, true);
+    }
 }
